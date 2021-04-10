7b3b482903981234ecf04fb81282399a83b1edc1
DorotaChroboczek <68025504+DorotaChroboczek@users.noreply.github.com>
2021-04-10 19:03:07 +0200

    feature: implement & register models: ActionType, ActionStatus, ActionEffect, Action

--- action_tracker/admin.py
+++ action_tracker/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+@admin.register(ActionType)
+class ActionTypeAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(ActionStatus)
+class ActionStatusAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(ActionEffect)
+class ActionEffectAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(Action)
+class ActionAdmin(admin.ModelAdmin):
+    list_display = ('searcher', 'company', 'position', 'type', 'date',
+                    'response_time', 'status', 'effect', 'comment', 'conclusions')
+
--- action_tracker/models.py
+++ action_tracker/models.py
@@ -1,3 +1,44 @@
-from django.db import models
+from django.db.models import *
 
-# Create your models here.
+from accounts.models import Profile
+
+
+class ActionType(Model):
+    name = CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class ActionStatus(Model):
+    name = CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class ActionEffect(Model):
+    name = CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+# in next step add field - proposed remuneration & negotiated salary value
+# change type, status, effect on_delete=SET_NULL
+
+class Action(Model):
+    searcher = ForeignKey(Profile, on_delete=CASCADE)
+    company = CharField(max_length=100, null=False, blank=False)
+    position = CharField(max_length=150, null=True, blank=True)
+    type = ForeignKey(ActionType, on_delete=CASCADE)
+    date = DateField()
+    response_time = DateField()
+    status = ForeignKey(ActionStatus, on_delete=CASCADE)
+    effect = ForeignKey(ActionEffect, on_delete=CASCADE)
+    comment = CharField(max_length=255, null=True, blank=True)
+    conclusions = CharField(max_length=255, null=True, blank=True)
+
+    def __str__(self):
+        return self.company
+
